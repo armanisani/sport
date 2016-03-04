40d9cb6e64d59f301549aae11fa1bd9b7b677057
Arman Sani <armanisani@gmail.com>
2016-03-04 15:25:58 -0800

    updated server.js added a couple routes

--- server.js
+++ server.js
@@ -4,3 +4,25 @@ var
   express = require('express'),
   logger = require('morgan'),
   app = express()
+  Team = require('./models/Team.js')
+// mongoose connection
+mongoose.connect('mongodb://localhost/als-sports', function(err){
+  if(err) return console.log(err)
+  console.log("mongoose connected to db")
+})
+// middleware
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+////////////////// Routes ////////////////////////
+
+app.get('/teams/', function(req,res){
+  Team.find({}, function(err, team){
+    if(err) return console.log(err)
+    res.json(team)
+  })
+})
+app.create('/teams/', function(req,res){
+  Team.create
+})
