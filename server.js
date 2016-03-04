3a6ea087e60eb9b43ea892b138c28ccd9b41eb0e
Arman Sani <armanisani@gmail.com>
2016-03-04 15:41:49 -0800

    Added listening port and fixed few errors in server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var
   bodyParser = require('body-parser'),
   express = require('express'),
   logger = require('morgan'),
-  app = express()
+  app = express(),
+  Player = require('./models/Player.js'),
   Team = require('./models/Team.js')
 // mongoose connection
 mongoose.connect('mongodb://localhost/als-sports', function(err){
@@ -24,8 +25,12 @@ app.get('/teams/', function(req,res){
   })
 })
 app.post('/teams/', function(req,res){
-  Team.create(res.body, function(err,team){
+  Team.create(req.body, function(err,team){
     if(err) return console.log(err)
     res.json(team)
   })
+})
+app.listen(3000, function(err){
+  if(err) return console.log(err)
+  console.log("Listening to port 3000");
 })
